b4e460ade58e924c6d1a4a9b745769d6b9cd48e8
Chase Terry <chase.terry87@gmail.com>
2021-10-28 10:14:48 -0500

    Laravel Install with Jetstream, Inertia, Vue, and Vuetify

---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <v-app>
+      <v-main>
+        <v-container>Hello Vuetify!</v-container>
+      </v-main>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Welcome",
+};
+</script>
